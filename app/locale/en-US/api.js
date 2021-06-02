6980a75a7f0a4a8bb69f830da5120757a245e23a
Musa Sutisna <mus4.sutisn4@gmail.com>
2021-06-02 16:53:35 +0700

    Add locale to handle resource

--- app/locale/en-US/api.js
+++ app/locale/en-US/api.js
@@ -1,6 +1,9 @@
 module.exports = {
   'api_id_required': 'api id could not be empty',
   'api_not_found': 'api could not be found',
-  'resource_id_require': 'resource id could not be empty',
-  'resource_not_found': 'resource could not be found'
+  'resource_id_required': 'resource id could not be empty',
+  'resource_not_found': 'resource could not be found',
+  'edit_resource_success': 'successful edit resource',
+  'edit_resource_failed': 'failed to edit resource',
+  'role_group_format': 'role group only allowed contain alpha numeric character'
 };
